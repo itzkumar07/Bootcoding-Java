9004229bbdad6809d30f4e1aac7fced14082231d
itzkumar07 <kumarkankam17@gmail.com>
2025-01-16 16:50:37 +0530

    variable and ArithmeticOperators leran programs

---
+++ src/VariablesExample.java
@@ -0,0 +1,24 @@
+class VariablesExample {
+    public static void main(String[] args) {
+        int age = 25;
+        // Integer variable
+
+        double height = 5.9;
+        // Double variable
+
+        char grade = 'A';
+        // Character variable
+
+        boolean isPass = true;
+        // Boolean variable
+
+        String name = "Rahul";
+        // String variable
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPass);
+    }
+}
