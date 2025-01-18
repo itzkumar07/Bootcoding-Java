16824e5b3582d0f1670fc337971db0e11ae0cac2
itzkumar07 <kumarkankam17@gmail.com>
2025-01-18 15:52:49 +0530

    Add LargestElement program to find largest and smallest numbers
    This commit introduces a Java program under `dayfour/arryas` package that identifies the largest and smallest elements in a predefined integer array. The program includes a simple loop for comparisons and prints the results to the console.

---
+++ src/dayfour/arryas/ReverseArray.java
@@ -0,0 +1,18 @@
+package dayfour.arryas;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6,7,8,9,10};
+
+        int [] revArr = new int[arr.length];
+        for (int i = 0; i< arr.length; i++){
+            revArr[i] = arr[arr.length-1-i];
+        }
+        for (int i = 0; i < revArr.length; i++){
+            System.out.println(revArr[i]);
+        }
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
