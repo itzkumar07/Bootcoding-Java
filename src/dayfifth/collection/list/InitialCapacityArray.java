fce3bd4bc6ee64eaa57930f90f5c2ec00f9c5290
itzkumar07 <kumarkankam17@gmail.com>
2025-01-20 15:38:46 +0530

    Refactor list handling and remove unused recursive method
    Replaced `System.out` static import with direct usage for clarity. Removed the unused `Data` method and redundant code to enhance readability and maintainability. Simplified logic in `InitialCapacityArrayMain` to avoid unnecessary method calls.

--- src/dayfifth/collection/list/InitialCapacityArray.java
+++ src/dayfifth/collection/list/InitialCapacityArray.java
@@ -1,27 +1,19 @@
 package dayfifth.collection.list;
 
 import java.util.ArrayList;
-import static java.lang.System.*;
 
 public class InitialCapacityArray {
     int initialCapacity = 10;
-
     ArrayList<Integer> list = new ArrayList<>(initialCapacity);
 
     public void add(int i){
         list.add(i);
-        out.println(list.size());
+        System.out.println(list.size());
     }
 
     public void print(){
         for(int i : list){
-            out.println(i);
-        }
-    }
-
-    public static void Data( int i){
-        if(i<100){
-            Data(i+1);
+            System.out.println(i);
         }
     }
 }
