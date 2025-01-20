caf89d02745aa730ee67397a596dbdf890c6e83f
itzkumar07 <kumarkankam17@gmail.com>
2025-01-20 15:19:24 +0530

    Add elements and implement recursion in InitialCapacityArray
    Added methods to populate the array, print its contents, and introduced a recursive `Data` method. Optimized imports to use `System.out` statically for cleaner code.

--- src/dayfifth/collection/list/InitialCapacityArray.java
+++ src/dayfifth/collection/list/InitialCapacityArray.java
@@ -1,19 +1,27 @@
 package dayfifth.collection.list;
 
 import java.util.ArrayList;
+import static java.lang.System.*;
 
 public class InitialCapacityArray {
     int initialCapacity = 10;
+
     ArrayList<Integer> list = new ArrayList<>(initialCapacity);
 
     public void add(int i){
         list.add(i);
-        System.out.println(list.size());
+        out.println(list.size());
     }
 
     public void print(){
         for(int i : list){
-            System.out.println(i);
+            out.println(i);
+        }
+    }
+
+    public static void Data( int i){
+        if(i<100){
+            Data(i+1);
         }
     }
 }
--- src/dayfifth/collection/list/InitialCapacityArrayMain.java
+++ src/dayfifth/collection/list/InitialCapacityArrayMain.java
@@ -3,6 +3,12 @@ package dayfifth.collection.list;
 public class InitialCapacityArrayMain extends InitialCapacityArray {
     public static void main(String[] args) {
     InitialCapacityArrayMain initialCapacityArray = new InitialCapacityArrayMain();
+    initialCapacityArray.add(1);
+    initialCapacityArray.add(2);
+    initialCapacityArray.add(3);
+
+    initialCapacityArray.print();
+    initialCapacityArray.Data(2);
 
     }
 }
