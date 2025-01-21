87dc91c69cef76987f2af79600cc5c322390f193
itzkumar07 <kumarkankam17@gmail.com>
2025-01-21 20:25:43 +0530

    Add blank line printing using System.out.println()

--- src/daysixth/interfacesimpleproject/InterfaceExample.java
+++ src/daysixth/interfacesimpleproject/InterfaceExample.java
@@ -1,10 +1,14 @@
 package daysixth.interfacesimpleproject;
 
+import static java.lang.System.*;
+
 public class InterfaceExample {
     public static void main(String[] args) {
         Animal dog = new Dog();
         dog.eat();
         dog.sleep();
+
+        out.println();
 
         Pet cat = new Cat();
         cat.eat();
