242851a78363f3b7f195d4fa5f152eb8b7633a36
itzkumar07 <kumarkankam17@gmail.com>
2025-01-22 12:21:06 +0530

    Create and standardize new classes with abstract methods
    Added `AbstractArraysList` and its subclasses for improved modular design and array operations. Corrected class naming issues, standardized `System.out` with static imports, and enhanced readability in existing constructors. These changes improve consistency, functionality, and code structure.

---
+++ src/daysventh/AbstractArraysListMain.java
@@ -0,0 +1,13 @@
+package daysventh;
+
+import static java.lang.System.*;
+
+public class AbstractArraysListMain {
+    public static void main(String[] args) {
+        AbstractArraysListOne list1 = new AbstractArraysListOne();
+        out.println(list1);
+
+        AbstractArraysListTwo list2 = new AbstractArraysListTwo();
+        out.println(list2);
+    }
+}
--- src/daythree/ConstructorExample1.java
+++ src/daythree/ConstructorExample1.java
@@ -1,17 +1,19 @@
 package daythree;
 
+import static java.lang.System.*;
+
 public class ConstructorExample1 {
     int number;
     String text;
 
-    //Counstructor
+    //Constructor
     public ConstructorExample1(int number, String text){
         this.number = number;
         this.text = text;
     }
+
     public static void main(String[] args) {
         ConstructorExample1 obj = new ConstructorExample1(33,"Kumar");
-        System.out.println(obj.number+" "+obj.text);
-
+        out.println(obj.number+" "+obj.text);
     }
 }
