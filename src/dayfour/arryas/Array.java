2ff365f5fdff51bedac40d212f23259e6214f78e
itzkumar07 <kumarkankam17@gmail.com>
2025-01-24 14:01:14 +0530

    kk

--- src/dayfour/arryas/Array.java
+++ src/dayfour/arryas/Array.java
@@ -1,13 +1,13 @@
 package dayfour.arryas;
 
+import static java.lang.System.*;
 public class Array {
     public static void main(String[] args) {
-        int [] IntArrays = { 1,2,3,4,5,6,7,8,9,10 };
 
         int [] arr = new int[6]; //object creation in and
 
         for (int i = 0 ; i < arr.length; i++){
-            System.out.println(arr[i]);
+            out.println(arr[i]);
         }
     }
 }
