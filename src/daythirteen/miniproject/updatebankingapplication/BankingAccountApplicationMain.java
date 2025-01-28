64eb10941d609a95a7297fa4e348fa69028fa08f
itzkumar07 <kumarkankam17@gmail.com>
2025-01-28 14:32:13 +0530

    rename the method in BankAccountOperations.java file

--- src/daythirteen/miniproject/updatebankingapplication/BankingAccountApplicationMain.java
+++ src/daythirteen/miniproject/updatebankingapplication/BankingAccountApplicationMain.java
@@ -3,6 +3,6 @@ package daythirteen.miniproject.updatebankingapplication;
 public class BankingAccountApplicationMain {
     public static void main(String[] args) {
         BankAccountHandler bankAccountHandler = new BankAccountHandler();
-        bankAccountHandler.printAllvoidMethods();
+        bankAccountHandler.printMethods();
     }
 }
