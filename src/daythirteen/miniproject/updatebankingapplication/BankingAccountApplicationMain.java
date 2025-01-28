87928583a135bfd21ea068a9171b6bc32404838c
itzkumar07 <kumarkankam17@gmail.com>
2025-01-28 14:16:32 +0530

    New Project Create and udates the some Interface

---
+++ src/daythirteen/miniproject/updatebankingapplication/BankingAccountApplicationMain.java
@@ -0,0 +1,8 @@
+package daythirteen.miniproject.updatebankingapplication;
+
+public class BankingAccountApplicationMain {
+    public static void main(String[] args) {
+        BankAccountHandler bankAccountHandler = new BankAccountHandler();
+        bankAccountHandler.printAllvoidMethods();
+    }
+}
