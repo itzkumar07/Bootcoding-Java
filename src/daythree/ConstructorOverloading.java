4a42aaebdb4d4b9d7820f1d0271389af957adb2a
itzkumar07 <kumarkankam17@gmail.com>
2025-02-02 23:20:08 +0530

    Initial commit

--- src/daythree/ConstructorOverloading.java
+++ src/daythree/ConstructorOverloading.java
@@ -4,12 +4,20 @@ public class ConstructorOverloading {
     int number ;
     String text;
 
+<<<<<<< HEAD
     public ConstructorOverloading(){
+=======
+    ConstructorOverloading(){
+>>>>>>> 255f6e8 (Initial commit)
         number = 110;
         text = "bootcoding";
     }
 
+<<<<<<< HEAD
     public ConstructorOverloading(int number, String text){
+=======
+    ConstructorOverloading(int number, String text){
+>>>>>>> 255f6e8 (Initial commit)
         this.number = number;
         this.text = text;
     }
@@ -20,5 +28,9 @@ public class ConstructorOverloading {
 
         out.println(obj1.number+" "+ obj1.text);
         out.println(obj2.number+" "+obj2.text);
+<<<<<<< HEAD
+=======
+        out.println("--------------------------------------------------------------");  
+>>>>>>> 255f6e8 (Initial commit)
     }
 }
