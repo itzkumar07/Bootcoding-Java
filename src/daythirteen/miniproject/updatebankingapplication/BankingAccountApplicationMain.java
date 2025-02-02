64f9561d72500d5f0fd97dd8c562985eacd0f1a2
itzkumar07 <kumarkankam17@gmail.com>
2025-02-02 23:27:21 +0530

    Removed: commite line

--- src/daythirteen/miniproject/updatebankingapplication/BankingAccountApplicationMain.java
+++ src/daythirteen/miniproject/updatebankingapplication/BankingAccountApplicationMain.java
@@ -3,6 +3,6 @@ package daythirteen.miniproject.updatebankingapplication;
 public class BankingAccountApplicationMain {
     public static void main(String[] args) {
         BankAccountHandler bankAccountHandler = new BankAccountHandler();
-        bankAccountHandler.printMethods(); // Calling method to initiate the banking operations
+        bankAccountHandler.printMethods(); 
     }
 }
