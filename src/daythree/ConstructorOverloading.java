1b59babd57e94fbcb5010d8d30a93e76c809d68d
itzkumar07 <kumarkankam17@gmail.com>
2025-02-03 11:08:12 +0530

    Removed: some ovrrried methods remove

--- src/daythree/ConstructorOverloading.java
+++ src/daythree/ConstructorOverloading.java
@@ -4,22 +4,9 @@ public class ConstructorOverloading {
     int number ;
     String text;
 
-<<<<<<< HEAD
-    public ConstructorOverloading(){
-=======
     ConstructorOverloading(){
->>>>>>> 255f6e8 (Initial commit)
         number = 110;
         text = "bootcoding";
-    }
-
-<<<<<<< HEAD
-    public ConstructorOverloading(int number, String text){
-=======
-    ConstructorOverloading(int number, String text){
->>>>>>> 255f6e8 (Initial commit)
-        this.number = number;
-        this.text = text;
     }
 
     public static void main(String[] args) {
@@ -28,9 +15,7 @@ public class ConstructorOverloading {
 
         out.println(obj1.number+" "+ obj1.text);
         out.println(obj2.number+" "+obj2.text);
-<<<<<<< HEAD
-=======
         out.println("--------------------------------------------------------------");  
->>>>>>> 255f6e8 (Initial commit)
+
     }
 }
