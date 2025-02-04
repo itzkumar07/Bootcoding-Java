c17001b26f23ce6d568846d901f47d05a1a5fea6
itzkumar07 <kumarkankam17@gmail.com>
2025-02-04 17:02:53 +0530

    Formation the code

--- src/daythirteen/abstractionsimpleproject/MultipleAbstractsBigProgram.java
+++ src/daythirteen/abstractionsimpleproject/MultipleAbstractsBigProgram.java
@@ -1,24 +1,28 @@
 package daythirteen.abstractionsimpleproject;
 
-import static java.lang.System.*;
+import static java.lang.System.out;
 
+// Main class to run the program
 public class MultipleAbstractsBigProgram {
     public static void main(String[] args) {
-        Vehicle [] vehicles = new Vehicle[4];
-        vehicles[0]= new Car("Toyota", "Corolla");
-        vehicles[1] = new Motercycle("Harley-Davidson","Street 750");
-        vehicles[2]= new Airplane("Boeing","727");
-        vehicles[3]= new Helicopter("Bell","206");
+        // Create an array of vehicles (land and air vehicles)
+        Vehicle[] vehicles = new Vehicle[4];
+        vehicles[0] = new Car("Toyota", "Corolla");
+        vehicles[1] = new Motercycle("Harley-Davidson", "Street 750");
+        vehicles[2] = new Airplane("Boeing", "727");
+        vehicles[3] = new Helicopter("Bell", "206");
 
-        for (Vehicle vehicle: vehicles){
+        // Iterate through each vehicle and call their methods
+        for (Vehicle vehicle : vehicles) {
             vehicle.displayInfo();
             vehicle.startEngine();
 
-            if (vehicle instanceof LandVehicle){
+            if (vehicle instanceof LandVehicle) {
                 ((LandVehicle) vehicle).drive();
-            }else if (vehicle instanceof AirVehicle){
+            } else if (vehicle instanceof AirVehicle) {
                 ((AirVehicle) vehicle).fly();
             }
+
             vehicle.stopEngine();
             out.println("________________________________________");
         }
